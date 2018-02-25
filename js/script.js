988d71b012f0c14f9bf562189d43de2ff5415daf
Iam-madhusudhan <madhusudhan1863@gmail.com>
2018-02-25 19:34:20 +0530

    added overlay for images to click and open

--- js/script.js
+++ js/script.js
@@ -8,11 +8,13 @@ fetch(
 )
   .then(data => data.json())
   .then(gifs => {
-    console.log(gifs);
-    // layout.innerHTML=createMarkup(gifs,[h,v]);
     const digits = Array.from({ length: 50 }, () => {
-	  return [randomNumber(4), randomNumber(4)]    });
+      return [randomNumber(4), randomNumber(4)];
+    });
     layout.innerHTML = createMarkup(gifs, digits);
+    const items = document.querySelectorAll(".ct-grid__item");
+	items.forEach(item => item.addEventListener("click", handleClick));
+	overlayClose.addEventListener('click',closeOverlay);
   })
   .catch(err => {
     console.log(err);
@@ -21,17 +23,14 @@ fetch(
 function createMarkup(gifs, digits) {
   return gifs.data
     .map((value, index) => {
-	  const digit = digits[index];
-	  let height=[];
-	  height.push(value.images.original.height)
-	  
-  console.log(height);
-	  
+      const digit = digits[index];
+      let height = [];
+      height.push(value.images.original.height);
       return `
 			<div class="ct-grid__item h${digit[0]} v${digit[1]}" id="${value.id}">
 				<img class="ct-gif" src="${value.images.original.url}" alt=""/>
 				<div class="ct-grid__item--overlay">
-					<button>View</button>
+					<button>View&#8594;</button>
 				</div>
 			</div>`;
     })
@@ -42,3 +41,15 @@ function randomNumber(limit) {
   return Math.floor(Math.random() * limit) + 1;
 }
 
+function handleClick(e) {
+  console.log(e.currentTarget);
+  const src = e.currentTarget.querySelector("img").src;
+  overlayImage.src = src;
+  overlay.classList.add("ct-open");
+}
+
+function closeOverlay() {
+  overlay.classList.remove("ct-open");
+}
+
+// console.log(items);
